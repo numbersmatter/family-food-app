8ac9da6f2ab1521296a18157232d488724c573de
MarkMutt <lleasy8@gmail.com>
2024-08-09 18:53:45 -0400

    sign-in page

---
+++ app/components/shell/logo-image.tsx
@@ -0,0 +1,11 @@
+import charity from "~/images/charity.gif"
+
+export function LogoImg() {
+  return (
+    <img
+      className="mx-auto h-10 w-auto"
+      src={charity}
+      alt="Your Company"
+    />
+  )
+}
